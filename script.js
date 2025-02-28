1822a3e8e1c6965676a159bae048fa6659bf9aa5
ghosti777 <bczero3x@gmail.com>
2025-02-27 19:14:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function updateTimeframes() {
     let ltf, htf;
 
     // Correct LTF & HTF Assignments
-    if (midpoint === "3M") { ltf = "2M"; htf = "12M"; }
-    else if (midpoint === "5M") { ltf = "3M"; htf = "20M"; }
-    else if (midpoint === "15M") { ltf = "5M"; htf = "1H"; }
-    else if (midpoint === "30M") { ltf = "7M"; htf = "2H"; }
+    if (midpoint === "3M") { ltf = "1M"; htf = "15M"; }
+    else if (midpoint === "5M") { ltf = "2M"; htf = "30M"; }
+    else if (midpoint === "15M") { ltf = "3M"; htf = "1H"; }
+    else if (midpoint === "30M") { ltf = "5M"; htf = "2H"; }
     else if (midpoint === "1H") { ltf = "15M"; htf = "4H"; }
 
     // Update UI with the selected timeframes
